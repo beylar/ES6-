44e562af40cde8166f6ecdbd97a0aa358359a20b
Beyla Irakoze <beylaruz@gmail.com>
2024-05-12 14:04:51 +0200

    Using template literals to display a string / string interpolation

---
+++ Exercise/templateLiteral.js
@@ -0,0 +1,8 @@
+/*Your company website needs a dynamic welcome message. Wwrite a function greetUser(name) that uses template
+literals to create a personalised greeting message like "Welcome back, ${name}"*/
+
+function greetUser(name){
+    return `Welcome back, ${name}`
+}
+
+console.log(greetUser("Belyse"))
