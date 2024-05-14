95347e14e2791ee8712623987adfb71eeb04ac6d
Beyla Irakoze <beylaruz@gmail.com>
2024-05-14 19:31:44 +0200

    using set with the values to set values to properties

---
+++ gettersAndSetters.js
@@ -0,0 +1,57 @@
+class Person {
+    constructor(name) {
+        this.setName(name);
+    }
+    getName() {
+        return this.name;
+    }
+    setName(newName) {
+        newName = newName.trim();
+        if (newName === '') {
+            throw 'The name cannot be empty';
+        }
+        this.name = newName;
+    }
+}
+
+let person = new Person('Jane Doe');
+console.log(person); // Jane Doe
+
+person.setName('Jane Smith');
+console.log(person.getName()); // Jane Smith
+
+
+//Example 2: 
+let meeting = {
+    attendees: [],
+    add(attendee) {
+        console.log(`${attendee} joined the meeting.`);
+        this.attendees.push(attendee);
+        return this;
+    },
+    get latest() {
+        let count = this.attendees.length;
+        return count == 0 ? undefined : this.attendees[count - 1];
+    }
+};
+
+meeting.add('John').add('Jane').add('Peter');
+console.log(`The latest attendee is ${meeting.latest}.`);
+
+
+//Example 3
+class Person {
+    constructor(name) {
+        this._name = name;
+    }
+    get name() {
+        return this._name;
+    }
+}
+
+let persons = new Person("Jane Doe");
+console.log(persons.name);
+
+// attempt to change the name, but cannot
+persons.name = 'Jane Smith';
+console.log(persons.name); // Jane Doe
