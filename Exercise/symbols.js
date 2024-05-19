564bdac50d467743dc5ab1d23a4cb3421e14e64a
Beyla Irakoze <beylaruz@gmail.com>
2024-05-19 13:45:57 +0200

    symbols on object property

---
+++ Exercise/symbols.js
@@ -0,0 +1,16 @@
+// Symbolic Secrets: You're building a secure configuration system. Create a function secureConfig(config) 
+// that takes an object config and uses a Symbol as a key to store a private property named apiKey. Return the 
+// modified config object.
+
+
+function secureConfig(config){
+    const modify = Symbol("id");
+    config = {
+        [modify]: "left",
+        "age": "20"
+    }
+    return config;
+}
+
+
+console.log(secureConfig())
