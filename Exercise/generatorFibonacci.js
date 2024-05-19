bff4bfb82c4d66b6403bd34b7550b69f69b3fba2
Beyla Irakoze <beylaruz@gmail.com>
2024-05-19 13:31:45 +0200

    Fibonnacci

---
+++ Exercise/generatorFibonacci.js
@@ -0,0 +1,32 @@
+/*Generator fibonacci: numbers are fun! Write a generator function fibonacciGenerator(limit) that yields
+Fibonacci numbers up to a specified limit. Remember, the first two fibonacci numbers are 0 and 1 and each subsequent 
+number is the sum of the previous two.*/
+
+function * fibonacciGenerator(limit){
+   let num1 = 0;
+   let num2 = 1;
+   yield num1;
+   yield num2;
+   let next;
+    for(let i=1; i<=limit; ++i){
+        next = num1 + num2;
+        yield(next)
+        num1 = num2;
+        num2 = next;
+
+    }
+
+}
+
+let fibonacci = fibonacciGenerator(10)
+console.log(fibonacci.next().value)
+console.log(fibonacci.next().value)
+console.log(fibonacci.next().value)
+console.log(fibonacci.next().value)
+console.log(fibonacci.next().value)
+console.log(fibonacci.next().value)
+console.log(fibonacci.next().value)
+console.log(fibonacci.next().value)
+console.log(fibonacci.next().value)
+console.log(fibonacci.next().value)
+
