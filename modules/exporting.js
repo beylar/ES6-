82f076810a4bf14823684095f7383fa7dc76b336
Beyla Irakoze <beylaruz@gmail.com>
2024-05-26 20:23:01 +0200

    Scheduling function and time

--- modules/exporting.js
+++ modules/exporting.js
@@ -1,5 +1,8 @@
 export default function greet (name){
     return `Hello ${name}, welcome back`
 }
-
+export function schedule (time){
+    return `Welcome to the ${time} meeting.`
+}
+console.log(schedule(9))
 console.log(greet("charite"))
